1b26ee85dbc8ad9e90cec1bdbd91ac64156cc1f8
Yi Xiong Ng <yixiongng@gmail.com>
2020-07-29 15:40:06 +0800

    remove dotenv calling

--- templates/contactUs.php
+++ templates/contactUs.php
@@ -1,15 +1,6 @@
-<?php
-    require_once('vendor/autoload.php');
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-
-    $contactPerson =  $_ENV['contactPerson'];
-    $contactNum =  $_ENV['contactNumber'];
-?>
-
 <html>
     <body>
         <h1>Contact Us</h1>
-        <p>please contact <?php echo $contactPerson?> at <?php echo $contactNum?></p>
+        <p>please contact <?php echo $contactPerson?> at <?php echo $contactNumber?></p>
     </body>
 </html>
